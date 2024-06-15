5e56cf0f5a4bcd50fdde42775e3e2186781704c7
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-15 22:16:09 +0700

    set static files and view engine

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// sets
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middlewares
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/', require('./routes'));
