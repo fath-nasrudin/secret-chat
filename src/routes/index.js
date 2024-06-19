03ab44c91e2dc33c3ea518d94f6d509f2278a6a0
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-06-19 08:58:43 +0700

    Add default values for local variables to prevent undefined error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+router.use((req, res, next) => {
+  res.locals = {
+    errors: null,
+    user: req.user ? req.user: null,
+    userdata: null,
+  }
+  next();
+})
+
 router.route('/')
   .get((req, res) => {
     res.send('Root Page');
